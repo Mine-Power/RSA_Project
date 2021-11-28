47267feb7e41af8a86dae73b89ba19bffbc91bf6
Duc Truong <ducqt00@gmail.com>
2021-11-28 23:40:52 +0800

    Reverting test

--- main.py
+++ main.py
@@ -18,22 +18,25 @@ def main():
             "Please enter the number of bits for n (512 or 1024 or 2048): \n"
         )
     )
-    if userOptions == 1:
-        rsa_attack = attacks.RSA_Attack(noOfBits, True)
-        time = rsa_attack.perform_attack()[1]
-        print("Attack time: {t}".format(t=time))
-    elif userOptions == 2:
-        attacksParallel.init(noOfBits)
-        time = attacksParallel.perform_attack()[1]
-        print("Attack time: {t}".format(t=time))
-    elif userOptions == 3:
-        noOfIterations = int(
-            input("Please enter the number of iterations you want to run: \n")
-        )
-        atkStat = AttackStatistics()
-        atkStat.oracleQueryStatistic(noOfIterations, noOfBits)
-    else:
-        print("Unexpected input")
+    try:
+        if userOptions == 1:
+            rsa_attack = attacks.RSA_Attack(noOfBits, True)
+            time = rsa_attack.perform_attack()[1]
+            print("Attack time: {t}".format(t=time))
+        elif userOptions == 2:
+            attacksParallel.init(noOfBits)
+            time = attacksParallel.perform_attack()[1]
+            print("Attack time: {t}".format(t=time))
+        elif userOptions == 3:
+            noOfIterations = int(
+                input("Please enter the number of iterations you want to run: \n")
+            )
+            atkStat = AttackStatistics()
+            atkStat.oracleQueryStatistic(noOfIterations, noOfBits)
+        else:
+            print("Unexpected input")
+    except Exception:
+        print("There is an exception")
 
 
 if __name__ == "__main__":
